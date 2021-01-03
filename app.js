844d840ec95aec21f698327ae9cf81fbd549f5e5
Kai <m3sv.is.me@gmail.com>
2021-01-03 19:16:01 +1100

    Changed hours to be specifcally in my timezone

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const { default: axios } = require("axios");
 
 const cheerio = require("cheerio"),
-	// dotEnv = require("dotenv"),
+	dotEnv = require("dotenv"),
 	Discord = require("discord.js"),
 	{ exec } = require("child_process"),
 	client = new Discord.Client();
 
-// dotEnv.config();
+dotEnv.config();
 let myAccount = null;
 let canRun = false;
 let $ = null;
 let imgUrl = null;
 
-const downloadAndSend = async () => {
+const downloadAndSend = async (user) => {
 	let rainData = null;
 	// get rain radar image
 	await axios
@@ -28,7 +28,7 @@ const downloadAndSend = async () => {
 	exec("mkdir imgs");
 	exec(`cd imgs && curl ${imgUrl} --output img.gif`);
 
-	myAccount.send("Weather data:", { files: [__dirname + "/imgs/img.gif"] });
+	user.send("Weather data:", { files: [__dirname + "/imgs/img.gif"] });
 };
 
 client.on("ready", async () => {
@@ -38,17 +38,25 @@ client.on("ready", async () => {
 });
 
 client.on("message", async (msg) => {
-	if (!msg.author.bot && msg.content === "map") await downloadAndSend();
+	if (!msg.author.bot && msg.content === "map") await downloadAndSend(msg.author);
 });
 
 setInterval(async () => {
 	if (canRun) {
-		let hour = new Date().getHours();
+		let hourTime = new Date().toLocaleTimeString("en-AU", { timeZone: "Australia/Melbourne" });
+		let hour = "";
+		for (let i = 0; i < hourTime.length; i++) {
+			if (hourTime[i] != ":") {
+				hour += hourTime[i];
+				hour = parseInt(hour);
+				break;
+			}
+		}
 		let minute = new Date().getMinutes();
 		let second = new Date().getSeconds();
 
 		if (hour >= 8 && hour <= 20 && minute == 0 && second == 0) {
-			await downloadAndSend();
+			await downloadAndSend(myAccount);
 		}
 	}
 }, 1000);
